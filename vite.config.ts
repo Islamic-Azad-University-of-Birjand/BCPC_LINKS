af7c64a15265be084fb56410b6fe9b4ce37275a4
saeed0920 <sd.eed1381@gmail.com>
2024-12-03 11:24:47 +0330

    try to fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
   },
-  base: "BCPC_LINKS",
+  base: "./",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
